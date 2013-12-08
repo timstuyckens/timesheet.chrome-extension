5978a3f6f9d64e610593af966b8983e1886d47a2
Tim Stuyckens <timstuyckens@gmail.com>
2013-12-08 15:22:29 +0100

    published version to store. Tryout to get identity
    refactored callback geolocation with Deferred

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,37 +2,66 @@
 
 var manifest = chrome.runtime.getManifest();
 
-var postGeoLocation=function(position){
-	//console.log(position.coords);
-	var loc=[position.coords.latitude,position.coords.longitude];
-
-	var url="http://timesheetservice.herokuapp.com/entry"
-	//var url="http://localhost:3000/entry"
-	var data = { 
-		type: 'locationinfo', 
-		userinfo: { 
-			deviceid: manifest.name+"-"+manifest.version, 
-			devicetype: 'Chrome'
-		},
-		loc: loc
-	};
-
-	$.post(url,data).done(function(d){
-		console.table("POST SUCCES",d);
-	}).fail(function(a,b,c,d){
-		console.log("POST failed",a,b,c,d);
+if(chrome.identity.onSignInChanged){	
+	//currently only in canary
+	chrome.identity.onSignInChanged.addListener(function(identity){
+		console.log("Identity changed",identity);
 	});	
+}
+
+var tryGetCurrentUser=function(){	
+	//http://developer.chrome.com/extensions/app_identity.html#update_manifest
+	var dfd=new $.Deferred();
+	chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+		if(token){
+			console.log(token);
+		}	
+	 	dfd.resolve("");
+	});
+	return dfd;
 };
-var logError=function(err){
-	console.log("Error getting current position",err);
+var getLocation=function(){
+	var dfd=new $.Deferred();
+	navigator.geolocation.getCurrentPosition(function(position){
+		dfd.resolve(position.coords);
+	}, function(err){
+		dfd.reject(err);
+		console.log("Error getting current position",err);
+	});
+	return dfd;
+}
+
+var postGeoLocation=function(){
+
+	$.when(tryGetCurrentUser(),getLocation()).then(function(user,coords){
+		var loc=[coords.latitude,coords.longitude];
+		var url="http://timesheetservice.herokuapp.com/entry"
+		//var url="http://localhost:3000/entry"
+		var data = { 
+			type: 'locationinfo', 
+			userinfo: { 
+				deviceid: manifest.name+"-"+manifest.version, 
+				devicetype: 'Chrome',
+				mail:user
+			},
+			loc: loc
+		};
+
+		$.post(url,data).done(function(d){
+			console.table("POST SUCCES",d,data);
+		}).fail(function(a,b,c,d){
+			console.log("POST failed",a,b,c,d);
+		});	
+
+	});
 };
 
 
 var alarmKey="servicePostAlarm";
-var alarmInfo={periodInMinutes : 0.1};
+var alarmInfo={periodInMinutes :0.1 };
 var alarmCallbacks={};
 alarmCallbacks[alarmKey]=function(){
-	navigator.geolocation.getCurrentPosition(postGeoLocation, logError);
+	postGeoLocation();
 };
 
 var alarm=chrome.alarms.create(alarmKey, alarmInfo);
